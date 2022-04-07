bc854d91e6f1df896e2f85fae178910894216dee
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-04-08 00:17:58 +0900

    :label: add Service interfaces

---
+++ api/src/main/java/com/example/api/core/recommendation/RecommendationService.java
@@ -0,0 +1,16 @@
+package com.example.api.core.recommendation;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface RecommendationService {
+
+  @GetMapping(
+      value = "/recommendation",
+      produces = "application/json"
+  )
+  List<Recommendation> getRecommendations(
+      @RequestParam(value = "productId") int productId
+  );
+}
