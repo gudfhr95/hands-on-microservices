dec817b9f4e7c94257ff3c811fc51360141f3790
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-04-08 00:54:21 +0900

    :sparkles: add ExceptionHandler

--- util/src/main/java/com/example/util/http/HttpErrorInfo.java
+++ util/src/main/java/com/example/util/http/HttpErrorInfo.java
@@ -6,21 +6,21 @@ import org.springframework.http.HttpStatus;
 public class HttpErrorInfo {
 
   private final ZonedDateTime timestamp;
-  private final String path;
   private final HttpStatus httpStatus;
+  private final String path;
   private final String message;
 
   public HttpErrorInfo() {
     timestamp = null;
-    path = null;
     httpStatus = null;
+    path = null;
     message = null;
   }
 
-  public HttpErrorInfo(String path, HttpStatus httpStatus, String message) {
+  public HttpErrorInfo(HttpStatus httpStatus, String path, String message) {
     timestamp = ZonedDateTime.now();
-    this.path = path;
     this.httpStatus = httpStatus;
+    this.path = path;
     this.message = message;
   }
 
@@ -28,12 +28,12 @@ public class HttpErrorInfo {
     return timestamp;
   }
 
-  public String getPath() {
-    return path;
-  }
-
   public HttpStatus getHttpStatus() {
     return httpStatus;
+  }
+
+  public String getPath() {
+    return path;
   }
 
   public String getMessage() {
