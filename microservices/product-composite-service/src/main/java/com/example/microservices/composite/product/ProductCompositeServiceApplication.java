2dc7b03a9f03a3606ed944f04f87a437b60b7f32
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-04-17 19:58:47 +0900

    :wrench: add Swagger config

--- microservices/product-composite-service/src/main/java/com/example/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/com/example/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,14 +1,61 @@
 package com.example.microservices.composite.product;
 
+import static java.util.Collections.emptyList;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 @ComponentScan("com.example")
 public class ProductCompositeServiceApplication {
+
+  @Value("${api.common.version}")
+  String apiVersion;
+  @Value("${api.common.title}")
+  String apiTitle;
+  @Value("${api.common.description}")
+  String apiDescription;
+  @Value("${api.common.termsOfServiceUrl}")
+  String apiTermOfServiceUrl;
+  @Value("${api.common.license}")
+  String apiLicense;
+  @Value("${api.common.licenseUrl}")
+  String apiLicenseUrl;
+  @Value("${api.common.contact.name}")
+  String apiContactName;
+  @Value("${api.common.contact.url}")
+  String apiContactUrl;
+  @Value("${api.common.contact.email}")
+  String apiContactEmail;
+
+  @Bean
+  public Docket apiDocumentation() {
+    return new Docket(SWAGGER_2)
+        .select()
+        .apis(basePackage("com.example.microservices.composite.product"))
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(new ApiInfo(
+            apiTitle,
+            apiDescription,
+            apiVersion,
+            apiTermOfServiceUrl,
+            new Contact(apiContactName, apiContactUrl, apiContactEmail),
+            apiLicense,
+            apiLicenseUrl,
+            emptyList()
+        ));
+  }
 
   @Bean
   RestTemplate restTemplate() {
