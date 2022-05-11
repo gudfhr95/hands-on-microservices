d5599bd4fe042a296904204ebc6796b1d93eda4e
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-05-11 22:16:22 +0900

    :sparkles: add ReviewRepository to ReviewService

--- microservices/review-service/src/main/java/com/example/microservices/core/review/ReviewServiceApplication.java
+++ microservices/review-service/src/main/java/com/example/microservices/core/review/ReviewServiceApplication.java
@@ -1,14 +1,23 @@
 package com.example.microservices.core.review;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.example")
+@Slf4j
 public class ReviewServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ReviewServiceApplication.class, args);
+    ConfigurableApplicationContext ctx = SpringApplication.run(
+        ReviewServiceApplication.class,
+        args
+    );
+
+    String mysqlUri = ctx.getEnvironment().getProperty("spring.datasource.url");
+    log.info("Connected to MySQL: {}", mysqlUri);
   }
 }
--- microservices/review-service/src/main/java/com/example/microservices/core/review/services/ReviewServiceImpl.java
+++ microservices/review-service/src/main/java/com/example/microservices/core/review/services/ReviewServiceImpl.java
@@ -2,22 +2,44 @@ package com.example.microservices.core.review.services;
 
 import com.example.api.core.review.Review;
 import com.example.api.core.review.ReviewService;
+import com.example.microservices.core.review.persistence.ReviewEntity;
+import com.example.microservices.core.review.persistence.ReviewRepository;
 import com.example.util.exceptions.InvalidInputException;
 import com.example.util.http.ServiceUtil;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ReviewServiceImpl implements ReviewService {
 
-  private static final Logger LOG = LoggerFactory.getLogger(ReviewServiceImpl.class);
-
+  private final ReviewRepository repository;
+  private final ReviewMapper mapper;
   private final ServiceUtil serviceUtil;
+
+  @Override
+  public Review createReview(Review body) {
+    try {
+      ReviewEntity entity = mapper.apiToEntity(body);
+      ReviewEntity newEntity = repository.save(entity);
+
+      log.debug(
+          "createReview: created a review entity: {}/{}",
+          body.getProductId(),
+          body.getReviewId()
+      );
+
+      return mapper.entityToApi(newEntity);
+    } catch (DataIntegrityViolationException e) {
+      throw new InvalidInputException(
+          "Duplicate key, Product Id: " + body.getProductId() + ", Review Id: "
+              + body.getReviewId());
+    }
+  }
 
   @Override
   public List<Review> getReviews(int productId) {
@@ -25,39 +47,22 @@ public class ReviewServiceImpl implements ReviewService {
       throw new InvalidInputException("Invalid productId: " + productId);
     }
 
-    if (productId == 213) {
-      LOG.debug("No reviews found for productId: {}", productId);
-      return new ArrayList<>();
-    }
+    List<ReviewEntity> entityList = repository.findByProductId(productId);
+    List<Review> list = mapper.entityListToApiList(entityList);
+    list.forEach(e -> e.setServiceAddress(serviceUtil.getServiceAddress()));
 
-    List<Review> list = new ArrayList<>();
-    list.add(new Review(
-        productId,
-        1,
-        "Author 1",
-        "Subject 1",
-        "Content 1",
-        serviceUtil.getServiceAddress()
-    ));
-    list.add(new Review(
-        productId,
-        2,
-        "Author 2",
-        "Subject 2",
-        "Content 2",
-        serviceUtil.getServiceAddress()
-    ));
-    list.add(new Review(
-        productId,
-        3,
-        "Author 3",
-        "Subject 3",
-        "Content 3",
-        serviceUtil.getServiceAddress()
-    ));
-
-    LOG.debug("/reviews response size: {}", list.size());
+    log.debug("getReviews: response size: {}", list.size());
 
     return list;
+  }
+
+  @Override
+  public void deleteReviews(int productId) {
+    log.debug(
+        "deleteReviews: tries to delete reviews for the product with productId: {}",
+        productId
+    );
+
+    repository.deleteAll(repository.findByProductId(productId));
   }
 }
