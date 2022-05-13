ee5bbd916e7219494cedd3cf5c0a8e78e3ca0708
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-05-13 23:18:56 +0900

    :sparkles: modify ReviewService to handle messages

--- api/src/main/java/com/example/api/core/review/ReviewService.java
+++ api/src/main/java/com/example/api/core/review/ReviewService.java
@@ -1,19 +1,12 @@
 package com.example.api.core.review;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Flux;
 
 public interface ReviewService {
 
-  @PostMapping(
-      value = "/review",
-      consumes = "application/json",
-      produces = "application/json"
-  )
   Review createReview(@RequestBody Review body);
 
   @GetMapping(
@@ -22,6 +15,5 @@ public interface ReviewService {
   )
   Flux<Review> getReviews(@RequestParam(value = "productId") int productId);
 
-  @DeleteMapping(value = "/review")
   void deleteReviews(@RequestParam(value = "productId") int productId);
 }
