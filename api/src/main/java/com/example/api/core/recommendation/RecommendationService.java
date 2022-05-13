6362bcf5f47f2d15a9929156a0e978461af89bf5
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-05-13 23:13:26 +0900

    :sparkles: modify RecommendationService to handle messages

--- api/src/main/java/com/example/api/core/recommendation/RecommendationService.java
+++ api/src/main/java/com/example/api/core/recommendation/RecommendationService.java
@@ -1,19 +1,12 @@
 package com.example.api.core.recommendation;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Flux;
 
 public interface RecommendationService {
 
-  @PostMapping(
-      value = "/recommendation",
-      consumes = "application/json",
-      produces = "application/json"
-  )
   Recommendation createRecommendation(@RequestBody Recommendation body);
 
   @GetMapping(
@@ -24,6 +17,5 @@ public interface RecommendationService {
       @RequestParam(value = "productId") int productId
   );
 
-  @DeleteMapping(value = "/recommendation")
   void deleteRecommendations(@RequestParam(value = "productId") int productId);
 }
