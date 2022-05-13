51ff55f613ddceabf3fb7b5121523a181cc22384
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-05-13 23:03:57 +0900

    :sparkles: modify ProductService to handle messages

--- api/src/main/java/com/example/api/core/product/ProductService.java
+++ api/src/main/java/com/example/api/core/product/ProductService.java
@@ -1,19 +1,12 @@
 package com.example.api.core.product;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import reactor.core.publisher.Mono;
 
 public interface ProductService {
 
-  @PostMapping(
-      value = "/product",
-      consumes = "application/json",
-      produces = "application/json"
-  )
   Product createProduct(@RequestBody Product body);
 
   @GetMapping(
@@ -22,6 +15,5 @@ public interface ProductService {
   )
   Mono<Product> getProduct(@PathVariable int productId);
 
-  @DeleteMapping(value = "/product/{productId}")
   void deleteProduct(@PathVariable int productId);
 }
